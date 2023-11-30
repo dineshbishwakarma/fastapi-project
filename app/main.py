dae9c847215ed9b9c706cbebb03fb460251b42d0
dineshbishwakarma <ghatanidinesh56@gmail.com>
2023-11-30 15:22:22 -0600

    Add CORS

--- app/main.py
+++ app/main.py
@@ -1,12 +1,29 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from . import models
 from .database import engine
 from .routers import post, user, auth, vote
 
 
+
 # models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+# List of domains allowed , * means all domains are allowed
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+
 
 # Include posts router
 app.include_router(post.router)
